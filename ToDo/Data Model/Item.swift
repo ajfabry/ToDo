2b245d9e916f128093f5a52f01ac74d0a841cfe5
ajfabry <ajfabry@sc.rr.com>
2019-03-13 01:32:52 -0400

    Convert to MVC design pattern

---
+++ ToDo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by AJ Fabry on 3/13/19.
+//  Copyright © 2019 Addison Fabry. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
